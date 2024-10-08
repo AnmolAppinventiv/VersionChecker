69326c4b69b9533f8286a5fda60d99850a348be7
user <anmol.aneja@appinventiv.com>
2024-10-08 16:42:39 +0530

    randommm

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,29 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 
 function App() {
-  const [currentVersion, setCurrentVersion] = useState();
-  const [previousVersion, setPreviousVersion] = useState();
   useEffect(() => {
-    setTimeout(() => {
-      const data = async () => {
-        const response = await fetch("/versions/currentVersion.json");
-        const data = await response.json();
-        setCurrentVersion(data.version);
-      };
-      if (currentVersion && previousVersion && currentVersion !== previousVersion) {
+    const data = async () => {
+      const response = await fetch("/versions/currentVersion.json");
+      const data = await response.json();
+      const currentVersion = data.version;
+
+      const previousVersion = localStorage.getItem("previousVersion");
+
+      if (currentVersion && currentVersion !== previousVersion) {
         window.location.reload();
       } else {
-        data();
+        localStorage.setItem("previousVersion", currentVersion);
       }
-    }, 0);
-  }, [currentVersion]);
+    };
 
-  console.log(currentVersion);
+    data();
+  }, []);
+
   return (
     <>
-      <h1>{currentVersion ? `CURRENT VERSION IS : ${currentVersion}` : ""}</h1>
       <h6>Appinventiv</h6>
     </>
   );
 }
-export default App;
 
-//purana version new version se compare krna hai 
-// agr difference hua toh page apne aap reload krwana hai 
+export default App;
