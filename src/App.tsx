1c719e459cce09e92274462aa4f8704197ceb3e1
user <anmol.aneja@appinventiv.com>
2024-10-08 15:29:24 +0530

    all code

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  const [currentVersion, setCurrentVersion] = useState(0);
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch("/versions/currentVersion.json");
+      const data = await response.json();
+      setCurrentVersion(data.version);
+    };
+
+    fetchData();
+  }, []);
+
+  console.log(currentVersion);
+  return (
+    <div>
+      <h1>{currentVersion ? `CURRENT VERSION IS : ${currentVersion}` : ""}</h1>
+    </div>
+  );
+}
+export default App;
