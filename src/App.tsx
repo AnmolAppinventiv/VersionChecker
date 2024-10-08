d770607fcf2f1976211b4573ca925435eb23fbc4
user <anmol.aneja@appinventiv.com>
2024-10-08 15:52:53 +0530

    random1

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,29 @@
 import { useEffect, useState } from "react";
 
 function App() {
-  const [currentVersion, setCurrentVersion] = useState(0);
+  const [currentVersion, setCurrentVersion] = useState();
   useEffect(() => {
     setTimeout(() => {
       const fetchData = async () => {
         const response = await fetch("/versions/currentVersion.json");
         const data = await response.json();
-
         setCurrentVersion(data.version);
       };
       fetchData();
-    }, 2000);
+    }, 1000);
   }, [currentVersion]);
 
   console.log(currentVersion);
   return (
-    <div>
+    <>
       <h1>{currentVersion ? `CURRENT VERSION IS : ${currentVersion}` : ""}</h1>
       <h2>Hello World</h2>
-    </div>
+      <p>Hell</p>
+    </>
   );
 }
 export default App;
+
+
+//purana version compare krna hai
+//
