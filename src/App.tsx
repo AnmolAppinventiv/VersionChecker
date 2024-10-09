43225c568cb210d7cfbec538e7a5cfd4fdc4da42
user <anmol.aneja@appinventiv.com>
2024-10-09 10:44:09 +0530

    hellllo

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,12 @@ function App() {
     const data = async () => {
       const response = await fetch("/versions/currentVersion.json");
       const data = await response.json();
+      debugger;
       const currentVersion = data.version;
       const previousVersion = localStorage.getItem("previousVersion");
-
       if (previousVersion && previousVersion != currentVersion) {
         localStorage.setItem("previousVersion", currentVersion);
-        window.location.reload();
+        // window.location.reload();
       } else if (!previousVersion) {
         localStorage.setItem("previousVersion", currentVersion);
       }
@@ -24,6 +24,7 @@ function App() {
       <h2>ram ram</h2>
       <p>hogya kya shayad</p>
       <h1>hello</h1>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero ipsam unde ullam?</p>
     </>
   );
 }
